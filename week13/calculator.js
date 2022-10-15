c4a21a9fa27daefa281c60e89f291e626bffef5d
AnastasiaANN <annastasia__alekseeva__2004@mail.ru>
2022-10-16 01:02:29 +0300

    added week13

---
+++ week13/calculator.js
@@ -0,0 +1,23 @@
+function plus(){ 
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert(+a + +b)
+}
+
+function minus(){
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert(a - b)
+}
+
+function multiplication(){
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert(a * b)
+}
+
+function division(){
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert(a / b)
+}
